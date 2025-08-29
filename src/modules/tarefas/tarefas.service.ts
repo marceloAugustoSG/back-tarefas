2f96c290cb780b4ec81e6d5aa46f5d9cfc76ec85
marceloAugustoSG <marcelo_augusto.soares@hotmail.com>
2025-08-29 11:47:46 -0300

    configurando build

--- src/modules/tarefas/tarefas.service.ts
+++ src/modules/tarefas/tarefas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Tarefa } from 'generated/prisma';
+import { Tarefa } from '@prisma/client';
 
 @Injectable()
 export class TarefasService {
